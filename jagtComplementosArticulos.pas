e3f6fab8676731f8aa3a6410911e45f0c1117f06
antonimusincognitus <antonimusincognitus@users.noreply.github.com>
2018-10-31 15:05:24 -0600

    20181031

--- jagtComplementosArticulos.pas
+++ jagtComplementosArticulos.pas
@@ -150,8 +150,8 @@ begin
   cxServidor :='192.168.4.9';
   cxProtocolo:='0';
   cxCarpeta  :='C:\Users\lumi\Documents\Cursos Delphi\bases de datos\';
-//  ConectarADB;}
-}
+//  ConectarADB;
+
 }
   edtClave.SetFocus;
 end;
